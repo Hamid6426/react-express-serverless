e86bc3af371f40b65f8b2ab2be1e5a4b9718883e
Hamid6426 <mianhamid6426@gmail.com>
2025-02-04 08:50:18 +0500

    initial commit 3

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
@@ -7,7 +8,10 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
 
   return {
-    plugins: [react()],
+    plugins: [
+      react(),
+      tailwindcss()
+    ],
     define: {
       __APP_ENV__: JSON.stringify(env.APP_ENV),
     },
